4cd23ad42395b7455df66e5ae30d019a27ca66d9
ko-dai0102 <koudai.tanno@gmail.com>
2023-10-09 00:23:51 +0900

    いいね機能改修

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,17 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_item
 
   def create
-    like = current_user.likes.build(item_id: params[:item_id])
-    like.save
-    redirect_to item_path(@item.id)
+    @like = Like.new(user_id: current_user.id, item_id: @item.id)
+    @like.save
+    render partial: 'items/like', locals: { item: @item }
   end
 
   def destroy
-    like = Like.find_by(item_id: params[:item_id], user_id: current_user.id)
-    like.destroy
-    redirect_to item_path(@item.id)
+    @like = Like.find_by(user_id: current_user.id, item_id: @item.id)
+    @like.destroy
+    render partial: 'items/like', locals: { item: @item }
   end
 
   private
